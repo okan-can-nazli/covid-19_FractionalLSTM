1791c7df928b71a6ad230a9dd4f01d5b6f67b850
okan-can-nazli <okancanazli@gmail.com>
2026-04-23 10:03:47 +0300

    added Normalization

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from CaputoLstm import LSTMCell
 import pandas as pd
+import numpy as np
 
-
-df = pd.read_csv("data.csv")
+########################################### SET THE DATA SET ###########################################
+df = pd.read_csv("WHO_data.csv")
 
 turkey = df[df["Country"] == "Türkiye"].copy() # copy just for safety to protect original data,nothing else
 turkey = turkey[["Date_reported", "New_cases", "Cumulative_cases", "New_deaths", "Cumulative_deaths"]] # only keep usefull columns
@@ -10,9 +11,42 @@ turkey["Date_reported"] = pd.to_datetime(turkey["Date_reported"]) # convert text
 
 turkey = turkey[turkey["New_cases"] >= 0] #!
 
+
+# #print(turkey.describe()) # we decided normalization with data values gap diffrence
+
+
+# Turkey switched from case-based to aggregate reporting after 2022
+# Post-2023 data is near-zero with no death records — not usable for training
+
+# print(turkey[turkey["Date_reported"] > "2023-01-01"].head(20)) 
+# print(turkey[turkey["Date_reported"] < "2023-01-01"].tail(40)) 
+
+
+turkey = turkey[turkey["Date_reported"] < "2023-01-01"]
+
+
+
+# print(turkey["Date_reported"].diff().value_counts())
+# gaps = turkey[turkey["Date_reported"].diff() == "7 days"]["Date_reported"]
+# print(gaps)
+
+# gaps = turkey[turkey["Date_reported"].diff() == "2 days"]["Date_reported"]
+# print(gaps)
+
+
+# March 2020 → June 2022    →  daily reporting   (807 rows)
+# 2022-06-05 first day of weekly data rows start
+# June 2022  → November 2022 →  weekly reporting  (24 rows)
+# First two rows (2020-03-12, 2020-03-14) have a 2-day gap — artifact of pandemic start, not a reporting issue
+
+
+turkey = turkey[turkey["Date_reported"] < "2022-06-05"] # remove data on weekly rows
+turkey = turkey.iloc[1:] # remove first day data (2 day gap)
+
 turkey = turkey.sort_values("Date_reported").reset_index(drop=True) # reset index of the data table
 
+########################################### NORMALIZATİON ###########################################
+turkey["New_cases"] = np.log1p(turkey["New_cases"]) # log(x+1) for x = 0 protection
+# Before:   min=0,  max=406,321,  std=28,954
+# After:    min=0.69,  max=11.6,  std=1.36
 
-print(turkey.shape)
-print(turkey.head())
-print(turkey.describe()) # we decided normalization with data values gap diffrence
